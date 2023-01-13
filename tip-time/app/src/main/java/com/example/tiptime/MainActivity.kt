9f9d8d357dbcfac6090385d3c800996de3d74548
dn717 <djiang3@lakeheadu.ca>
2023-01-13 12:19:01 -0500

    move tip-time to remote repo

---
+++ tip-time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding //lateinit申明顶级变量 将在多个方法中使用它
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)//初始化binding对象
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip=kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
+
+    }
+}
